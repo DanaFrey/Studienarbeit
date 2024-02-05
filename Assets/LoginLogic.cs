443bdf06cd66adb0d4c4179b7f8ea88fe55501bc
Dana Frey <inf21099@lehre.dhbw-stuttgart.de>
2024-02-05 17:01:03 +0100

    Start with Login/Register Implementation

---
+++ Assets/LoginLogic.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginLogic : MonoBehaviour
+{
+    public bool loggedIn = false;
+    public InputField username;
+    public InputField password;
+    public Button registerButton;
+    public GameObject loginScreenSignedOut;
+    public GameObject loginScreenSignedIn;
+
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("username", username.text);
+        form.AddField("password", password.text);
+        WWW www = new WWW("http://localhost/sqlconnect/register.php", form);
+        yield return www;
+
+        if (www.text == "0")
+        {
+            Debug.Log("User created successfully.");
+            //loginScreenSignedOut.SetActive(false);
+            //loginScreenSignedIn.SetActive(true);
+        }
+        else Debug.Log("User registration failed. Error #" + www.text);
+    }
+
+    public void VerifyInputs()
+    {
+        registerButton.interactable = (username.text.Length >= 5 && username.text.Length <= 16 && password.text.Length >= 6);
+    }
+}
