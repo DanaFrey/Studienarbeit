e161ea4f65f5f3c84be85df7c6d42f6281ee6c38
Dana Frey <inf21099@lehre.dhbw-stuttgart.de>
2024-02-05 14:20:29 +0100

    Working Log In Button

--- Assets/TitleScreenLogic.cs
+++ Assets/TitleScreenLogic.cs
@@ -7,16 +7,29 @@ public class TitleScreenLogic : MonoBehaviour
     [SerializeField] private VolumeSettings volumeSettings;
 
     public GameObject settingsScreen;
+    public GameObject loginScreen;
 
     public void Start()
     {
         volumeSettings.Start();
     }
-    public void openSettings(){
+    public void openSettings()
+    {
         settingsScreen.SetActive(true);
     }
 
-    public void closeSettings(){
+    public void closeSettings()
+    {
         settingsScreen.SetActive(false);
+    }
+
+    public void openLogin()
+    {
+        loginScreen.SetActive(true);
+    }
+
+    public void closeLogin()
+    {
+        loginScreen.SetActive(false);
     }
 }
